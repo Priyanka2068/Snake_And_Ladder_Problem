3b11f2f5e44b1d2ac8c41eb50ba73c7be95ba76f
chutki12 <priyankachaudhary2068>
2022-07-05 20:28:03 +0530

    REFACTORED: Player At Start Position

---
+++ src/com/bridgelabz/snakeLadder/SnakeAndLadder_1.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.snakeLadder;
+
+public class SnakeAndLadder_1 {
+
+	public static void main(String[] args) {
+		int start = 0;
+		System.out.println("Starting Position:" +start);
+	}
+
+}
