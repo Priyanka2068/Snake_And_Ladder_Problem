1b7c7f4835aad69772e0976637f27129d08da035
chutki12 <priyankachaudhary2068>
2022-07-05 20:38:00 +0530

    REFACTORED: To Get Number Between 1 To 6

---
+++ src/com/bridgelabz/snakeLadder/SnakeAndLadder_2.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.snakeLadder;
+
+public class SnakeAndLadder_2 {
+
+	public static void main(String[] args) {
+		int start = 0;
+		System.out.println("The start position :"+start);
+		int roll = (int) (Math.floor(Math.random() *10)%6+1);
+		System.out.println("After rolling the dice : "+ roll);
+	
+	}
+
+}
